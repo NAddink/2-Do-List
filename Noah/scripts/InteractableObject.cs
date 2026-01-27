838cf38f4dce865b7dd46055ba31db0b2b47a3cf
Noah Addink <noahaddink@gmail.com>
2026-01-26 18:15:53 -0700

    InteractableObject now has text offset, also using [tool] can visualize range and offset in editor

--- Noah/scripts/InteractableObject.cs
+++ Noah/scripts/InteractableObject.cs
@@ -4,13 +4,59 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-
+[Tool]
 public partial class InteractableObject : Node2D
 {
-    [Export] string LabelText;
+    [Export]
+    public string LabelText
+    {
+        get => _labelText;
+        set
+        {
+            _labelText = value;
+            UpdateLabelText();
+        }
+    }
+    private string _labelText = "";
 
-    [Export] float ActivationRange = 1;
-    [Export] float VisibleRange = 2;
+    [Export]
+    public float ActivationRange
+    {
+        get => _activationRange;
+        set
+        {
+            _activationRange = value;
+            UpdateActivationArea();
+        }
+    }
+    private float _activationRange = 1f;
+
+    [Export]
+    public float VisibleRange
+    {
+        get => _visibleRange;
+        set
+        {
+            _visibleRange = value;
+            UpdateVisibleArea();
+        }
+    }
+    private float _visibleRange = 2f;
+
+    [Export]
+    public float LabelYOffset
+    {
+        get => _labelYOffset;
+        set
+        {
+            _labelYOffset = value;
+            UpdateLabelOffset();
+        }
+    }
+    private float _labelYOffset = 0f;
+
+
+
     [Export] private InkStory inkData;
     private InkStory _story;            // runtime instance
 
@@ -28,14 +74,32 @@ public partial class InteractableObject : Node2D
 
     public override void _Ready()
     {
+        if (Engine.IsEditorHint())
+        {
+            EditorSetup();
+            return;
+        }
+
+        RuntimeSetup();
+    }
+
+    private void RuntimeSetup()
+    {
         // Attatch proceed logic to proceed signal
         GameManager.Instance.DialogProceed += DialogProceed;
 
+        // TEXT LABEL
+        RichTextLabel label = GetNode<RichTextLabel>("TextLabel");
         // get interaction icon and set visibility to false
-        GetNode<RichTextLabel>("TextLabel").Visible = false;
+        label.Visible = false;
 
         // set label to text to text specified in the field
-        GetNode<RichTextLabel>("TextLabel").Text = LabelText;
+        label.Text = LabelText;
+
+        // Set label offset
+        var pos = label.Position;
+        pos.Y = _labelYOffset;
+        label.Position = pos;
 
 
         // get activation area and scale based on range var
@@ -58,6 +122,8 @@ public partial class InteractableObject : Node2D
         // get reference to dialogUI 
         DialogUI = GetNode<DialogUI>("../UILayer/DialogUI");
         ChoiceUI = GetNode<ChoiceUI>("../UILayer/ChoiceUI");
+
+        
     }
 
     
@@ -234,6 +300,9 @@ public partial class InteractableObject : Node2D
 
     public override void _PhysicsProcess(double delta)
     {
+        if (Engine.IsEditorHint())
+            return;
+
         if(InteractCooldown > 0)
         {
             InteractCooldown -= (float)delta;
@@ -243,6 +312,72 @@ public partial class InteractableObject : Node2D
     public void OnInkVariableChanged(string name, bool value)
     {
         GameManager.Instance.AddFlag(name, value);
+    }
+
+
+
+
+    // Code for updating areas visually in editor for creating scenes
+    private void EditorSetup()
+    {
+        UpdateActivationArea();
+        UpdateVisibleArea();
+
+        // Optional: preview label text
+        var label = GetNodeOrNull<RichTextLabel>("TextLabel");
+        if (label != null)
+        {
+            label.Text = LabelText;
+            label.Visible = true;
+        }
+    }
+
+    // Helper methods for showing ranges and text in editor
+    private void UpdateActivationArea()
+    {
+        var shape = GetNodeOrNull<CollisionShape2D>(
+            "ActivationArea/CollisionShape2D"
+        )?.Shape as CircleShape2D;
+
+        if (shape != null)
+            shape.Radius = _activationRange;
+    }
+
+    private void UpdateVisibleArea()
+    {
+        var shape = GetNodeOrNull<CollisionShape2D>(
+            "VisibleArea/CollisionShape2D"
+        )?.Shape as CircleShape2D;
+
+        if (shape != null)
+            shape.Radius = _visibleRange;
+    }
+
+    private void UpdateLabelText()
+    {
+        if (!IsInsideTree())
+        return;
+
+        RichTextLabel label = GetNodeOrNull<RichTextLabel>("TextLabel");
+        if (label == null)
+            return;
+
+        label.Text = _labelText;
+
+    }
+
+        private void UpdateLabelOffset()
+    {
+        if (!IsInsideTree())
+            return;
+
+        var label = GetNodeOrNull<RichTextLabel>("TextLabel");
+        if (label == null)
+            return;
+
+        var pos = label.Position;
+        pos.Y = _labelYOffset;
+        label.Position = pos;
     }
 
 
