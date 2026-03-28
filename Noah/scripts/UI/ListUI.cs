c35a8b4383bc6ea8c3eccec18915eceb86fb27fd
Noah Addink <noahaddink@gmail.com>
2026-03-27 18:35:49 -0700

    Gated tasks

--- Noah/scripts/UI/ListUI.cs
+++ Noah/scripts/UI/ListUI.cs
@@ -60,70 +60,92 @@ public partial class ListUI : Control
 
     public void DisplayLabels()
     {
+        var list = GetNode<VBoxContainer>("VBoxContainer");
+
         // Clear existing items
-        var children = GetNode("ListItems").GetChildren();
-        foreach(Node child in children)
-        {
+        foreach (Node child in list.GetChildren())
             child.QueueFree();
-        }
 
-        
-        // Get list items shuffled
         Array<string> items = new Array<string>();
-        
+        Array<string> visibleItems = new Array<string>();
 
-        if(DescriptionText.Length != 0)
+        Random rand = new Random();
+
+        // Populate
+        if (DescriptionText.Length != 0)
         {
-            foreach(string s in DescriptionText.Split("\n"))
-                items.Add(s);
+            foreach (string s in DescriptionText.Split("\n"))
+            {
+                if (!string.IsNullOrWhiteSpace(s))
+                    items.Add(s);
+            }
         }
 
         items.Shuffle();
 
-        if (items.Count == 0) return;
-
-        int itemCnt = items.Count;
-
-        int maxY = 480;
-        int gap = (itemCnt > 1) ? maxY / (itemCnt - 1) : 0;
-
-        int yOffset = 0;
-        
-
-        Random rand = new Random();
-
+        // Filter visible items
         foreach (string item in items)
         {
+            string[] parts = item.Split("$$$");
+
+            if (parts.Length < 2)
+                continue;
+
+            if (parts.Length == 2)
+            {
+                visibleItems.Add(item);
+            }
+            else // Gated
+            {
+                bool visible = true;
+
+                for (int i = 1; i < parts.Length - 1; i++) // Check all items except first and last
+                {
+                    if (!GameManager.GetFlag(parts[i].Trim()))
+                    {
+                        visible = false;
+                        break;
+                    }
+                }
+
+                if (visible)
+                    visibleItems.Add(item);
+            }
+        }
+
+        if (visibleItems.Count == 0)
+            return;
+
+        // Create UI items
+        foreach (string item in visibleItems)
+        {
+            string[] parts = item.Split("$$$");
+
+            string itemName = parts[0].Trim();
+            string finalFlag = parts.Last().Trim();
+
+            // Wrapper VBoxContainer controls these which hold the labels
+            Control wrapper = new Control();
+            wrapper.CustomMinimumSize = new Vector2(0, 40); // row height 40
+
+            // Label
             RichTextLabel label = new RichTextLabel();
             label.BbcodeEnabled = true;
             label.AutowrapMode = TextServer.AutowrapMode.Off;
             label.HorizontalAlignment = HorizontalAlignment.Center;
             label.FitContent = true;
             label.ScrollActive = false;
+            label.CustomMinimumSize = new Vector2(990, 30);
 
-            // labels will match the width of the parent node to center properly
-            label.CustomMinimumSize = new Vector2(990, 30); 
-
-            string[] parts = item.Split("$$$");
-            if(parts.Length < 2)
-            {
-                GD.PrintErr("Todo list item " + parts[0] + " is missing an assigned flag.");
-                return;
-            }
-
-            string itemName = parts[0].Trim();
-            string itemFlag = parts[1].Trim();
-
-            // If flag is set to true - checked
-            if (GameManager.GetFlag(itemFlag))
+            // Style text
+            if (GameManager.GetFlag(finalFlag))
             {
                 label.Text =
                     "[s][color=DIM_GRAY]" +
                     itemName +
                     "[/color][/s]";
-
             }
-            else // unchecked
+            else
             {
                 label.Text =
                     "[shake rate=2.0 level=15 connected=1]" +
@@ -132,16 +154,19 @@ public partial class ListUI : Control
                     "[/wave][/shake]";
             }
 
-            // Center X around 300, shift randomly left/right
+            // Centering left-right
+            label.AnchorLeft = 0.5f;
+            label.AnchorRight = 0.5f;
+            label.OffsetLeft = -label.CustomMinimumSize.X / 2;
+            label.OffsetRight = label.CustomMinimumSize.X / 2;
+
+            // Apply left-right jitter
             int randXOffset = rand.Next(-50, 51);
+            label.Position += new Vector2(randXOffset, 0);
 
-
-            label.Position = new Vector2(randXOffset, yOffset);
-
-            yOffset += gap;
-
-            GetNode<Control>("ListItems").AddChild(label);
+            // Create labels
+            wrapper.AddChild(label);
+            list.AddChild(wrapper);
         }
-    
     }
 }
