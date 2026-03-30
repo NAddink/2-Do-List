09a023ff7f769a4ab1dc01cb56deb0ef2ab52544
Noah Addink <noahaddink@gmail.com>
2026-03-30 10:08:24 -0700

    User has to actually interact with object for tooltip to disappear

--- Noah/scripts/UI/TutorialTooltipUI.cs
+++ Noah/scripts/UI/TutorialTooltipUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Godot;
@@ -7,13 +8,19 @@ public partial class TutorialTooltipUI : Control
 
     private bool AllTutorialsCompleted = false;
 
+    private GameManager GameManager;
+
 
     // List of input buttons to be pressed during tutorial
     private Dictionary<string, bool> InputTutorials;
     private Dictionary<string, Sprite2D> InputSprites;
 
+    bool interactedWithObject;
+
     [Export]
     Sprite2D RightArrow, LeftArrow, UpArrow, DownArrow, Interact, TodoList;
+
+    
 
 
     public override void _Ready()
@@ -24,8 +31,7 @@ public partial class TutorialTooltipUI : Control
             { "move_up", false },
             { "move_down", false },
             { "move_left", false },
-            { "move_right", false }, 
-            { "interact", false }
+            { "move_right", false },
         };
 
         InputSprites = new Dictionary<string, Sprite2D>
@@ -34,17 +40,40 @@ public partial class TutorialTooltipUI : Control
             { "move_up", UpArrow },
             { "move_down", DownArrow },
             { "move_left", LeftArrow },
-            { "move_right", RightArrow }, 
-            { "interact", Interact }
+            { "move_right", RightArrow }
         };
+
+        GameManager = GetTree().Root.GetNode<GameManager>("GameManager");
+        GameManager.ToggleDialog += OnDialogEnter;
         
+    }
+
+    private void OnDialogEnter(bool isPaused)
+    {
+        if(interactedWithObject) return; // Skip if already interacted with object
+        
+        // This updates the interact button, separate from other input buttons
+        Sprite2D sprite = Interact;
+        AtlasTexture atlas = sprite.Texture as AtlasTexture;
+
+        if (atlas != null)
+        {
+            atlas = (AtlasTexture)atlas.Duplicate();
+            sprite.Texture = atlas;
+
+            Rect2 region = atlas.Region;
+            region.Position += new Vector2(64, 0);
+            atlas.Region = region;
+        }
+
+        interactedWithObject = true;
     }
 
     public override void _PhysicsProcess(double delta)
     {
         if (AllTutorialsCompleted) return;
 
-        if (!InputTutorials.ContainsValue(false)) // Check if all are completed
+        if (!InputTutorials.ContainsValue(false) && interactedWithObject) // Check if all are completed
         {
             AllTutorialsCompleted = true;
             
@@ -60,15 +89,15 @@ public partial class TutorialTooltipUI : Control
                 InputTutorials[action] = true;
                 GD.Print("Action " + action + " completed for first time!");
                 // Hide specific label for this tutorial item
-                var sprite = InputSprites[action];
-                var atlas = sprite.Texture as AtlasTexture;
+                Sprite2D sprite = InputSprites[action];
+                AtlasTexture atlas = sprite.Texture as AtlasTexture;
 
                 if (atlas != null)
                 {
                     atlas = (AtlasTexture)atlas.Duplicate();
                     sprite.Texture = atlas;
 
-                    var region = atlas.Region;
+                    Rect2 region = atlas.Region;
                     region.Position += new Vector2(64, 0);
                     atlas.Region = region;
                 }
